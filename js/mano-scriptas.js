98eb59273ba45700e562ed4ee93328e4b9b69de7
Anzelika Savickiene <anzelika@savickiene@codeacademylt.onmicrosoft.com>
2022-12-12 15:24:16 +0200

    mano pirmasis komitas

---
+++ js/mano-scriptas.js
@@ -0,0 +1,15 @@
+let a =1;
+console.log('Kintamojo a reiksme:' + a);
+a = 2;
+console.log('Kintamojo a reiksme:' +a);
+
+
+
+let vardas pavarde = 'Anzelika Savickiene';
+console.log('Mano vardas ir pavarde:' + vardas pavarde);
+
+
+let metai = 1977;
+let menuo = 7;
+let diena = 19;
+let gmmd = gimimo data: ' + diena +'
